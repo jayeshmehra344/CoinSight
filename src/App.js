0a146fc0e79e98f66fe5d280709e02f3fbd7fee2
jayeshmehra344 <jayeshmehra344@gmail.com>
2025-02-13 10:06:00 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { createTheme, ThemeProvider } from "@mui/material";
-import { HashRouter as Router, Route, Routes } from "react-router-dom"; // Changed to HashRouter
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Coin from "./pages/Coin";
 import Compare from "./pages/Compare";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 import Watchlist from "./pages/Watchlist";
-import { ToastContainer } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useEffect } from "react";
 
@@ -19,46 +19,39 @@ function App() {
     },
   });
 
+  var cursor;
+  var cursorPointer;
+
   useEffect(() => {
-    const cursor = document.getElementById("cursor");
-    const cursorPointer = document.getElementById("cursor-pointer");
+    cursor = document.getElementById("cursor");
+    cursorPointer = document.getElementById("cursor-pointer");
 
-    const handleMouseMove = (e) => {
-      if (cursor && cursorPointer) {
-        cursor.style.left = `${e.clientX}px`;
-        cursor.style.top = `${e.clientY}px`;
-        cursorPointer.style.left = `${e.clientX}px`;
-        cursorPointer.style.top = `${e.clientY}px`;
-      }
-    };
+    document.body.addEventListener("mousemove", function (e) {
+      return (
+        (cursor.style.left = e.clientX + "px"),
+        (cursor.style.top = e.clientY + "px"),
+        (cursorPointer.style.left = e.clientX + "px"),
+        (cursorPointer.style.top = e.clientY + "px")
+      );
+    });
 
-    const handleMouseDown = () => {
-      if (cursor && cursorPointer) {
-        cursor.style.height = "0.5rem";
-        cursor.style.width = "0.5rem";
-        cursorPointer.style.height = "3rem";
-        cursorPointer.style.width = "3rem";
-      }
-    };
+    document.body.addEventListener("mousedown", function (e) {
+      return (
+        (cursor.style.height = "0.5rem"),
+        (cursor.style.width = "0.5rem"),
+        (cursorPointer.style.height = "3rem"),
+        (cursorPointer.style.width = "3rem")
+      );
+    });
 
-    const handleMouseUp = () => {
-      if (cursor && cursorPointer) {
-        cursor.style.height = "0.3rem";
-        cursor.style.width = "0.3rem";
-        cursorPointer.style.height = "2rem";
-        cursorPointer.style.width = "2rem";
-      }
-    };
-
-    document.body.addEventListener("mousemove", handleMouseMove);
-    document.body.addEventListener("mousedown", handleMouseDown);
-    document.body.addEventListener("mouseup", handleMouseUp);
-
-    return () => {
-      document.body.removeEventListener("mousemove", handleMouseMove);
-      document.body.removeEventListener("mousedown", handleMouseDown);
-      document.body.removeEventListener("mouseup", handleMouseUp);
-    };
+    document.body.addEventListener("mouseup", function (e) {
+      return (
+        (cursor.style.height = "0.3rem"),
+        (cursor.style.width = "0.3rem"),
+        (cursorPointer.style.height = "2rem"),
+        (cursorPointer.style.width = "2rem")
+      );
+    });
   }, []);
 
   return (
@@ -67,7 +60,7 @@ function App() {
       <div className="cursor-pointer" id="cursor-pointer" />
       <ToastContainer />
       <ThemeProvider theme={theme}>
-        <Router>
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/dashboard" element={<Dashboard />} />
@@ -75,7 +68,7 @@ function App() {
             <Route path="/compare" element={<Compare />} />
             <Route path="/watchlist" element={<Watchlist />} />
           </Routes>
-        </Router>
+        </BrowserRouter>
       </ThemeProvider>
     </div>
   );
